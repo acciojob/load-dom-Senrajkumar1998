70722b70534382703cadeba27dee542bf941a9cb
Raju kumar <117720041+Senrajkumar1998@users.noreply.github.com>
2023-10-19 15:37:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/your JS code here. If required.
-let p=document.createElement('h1');
-p.innerText='DOM load success';
-document.body.appendChild(p);
+//your JS code here. If required.
+const para = document.createElement("p");
+para.innerText = "DOM load success";
+document.body.appendChild(para);
