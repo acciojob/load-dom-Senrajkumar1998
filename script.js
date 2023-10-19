a986ac76f976e60345957ab66c8e23dc4dfddc47
Raju kumar <117720041+Senrajkumar1998@users.noreply.github.com>
2023-10-19 15:34:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 /your JS code here. If required.
-let p=document.createElement("body");
-p.innerText="DOM load success";
+let p=document.createElement('h1');
+p.innerText='DOM load success';
+document.body.appendChild(p);
